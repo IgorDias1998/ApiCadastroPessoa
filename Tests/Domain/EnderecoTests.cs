5ec4f9e1d475fffb7a1f16f58dbd688e8395e5d8
IgorDias1998 <igordiass1998@gmail.com>
2025-12-27 21:55:12 -0300

    - Exemplos de cobertura de testes

---
+++ Tests/Domain/EnderecoTests.cs
@@ -0,0 +1,26 @@
+﻿using ApiCadastroPessoa.Domain.Exceptions;
+using ApiCadastroPessoa.Domain.ValueObjects;
+using Xunit;
+
+namespace ApiCadastroPessoa.Tests.Domain
+{
+    public class EnderecoTests
+    {
+        [Fact]
+        public void Deve_lancar_excecao_quando_cep_for_invalido()
+        {
+            Assert.Throws<DomainExceptions>(() =>
+                new Endereco(
+                    "123",
+                    "Rua Teste",
+                    "Centro",
+                    "São Paulo",
+                    "São Paulo",
+                    "10",
+                    null
+                )
+            );
+        }
+    }
+}
+
---
+++ Tests/Domain/PessoaTests.cs
@@ -0,0 +1,35 @@
+﻿using ApiCadastroPessoa.Domain.Entities;
+using ApiCadastroPessoa.Domain.Exceptions;
+using ApiCadastroPessoa.Domain.ValueObjects;
+using Xunit;
+
+namespace ApiCadastroPessoa.Tests.Domain
+{
+    public class PessoaTests
+    {
+        [Fact]
+        public void Deve_lancar_excecao_quando_nome_for_vazio()
+        {
+            var endereco = new Endereco(
+                "01001000",
+                "Rua Teste",
+                "Centro",
+                "São Paulo",
+                "São Paulo",
+                "10",
+                null
+            );
+
+            Assert.Throws<DomainExceptions>(() =>
+                new Pessoa(
+                    "",
+                    "email@email.com",
+                    DateTime.Now.AddYears(-20),
+                    "11999999999",
+                    endereco
+                )
+            );
+        }
+    }
+}
+
